7ddd8f93ac674ebcdaf3a4e0be96b98284798455
umeshgoyal-coder <goyalumesh73@gmail.com>
2022-09-13 11:38:54 +0530

    add done

---
+++ decorator_1.py
@@ -0,0 +1,15 @@
+import datetime
+import pytz
+#import date
+#tz1=pytz.timezone("America/New_York")
+#o1=datetime.datetime.now(tz1)
+#print(o1.strftime("%m/%d/%Y"))
+
+#date="2022 June,21"
+#o2=datetime.datetime.strptime(date,"%Y %B,%d")
+#print(o2)
+#print(type(o2))
+
+today=datetime.datetime.now()
+print(today)
+
